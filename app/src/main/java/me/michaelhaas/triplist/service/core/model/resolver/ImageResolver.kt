e274da9ed0067f9117c51c09f684c25bf1759c5b
TripleFrequency <triplefrequency@triplefrequency.com>
2019-05-04 20:24:27 -0400

    Use Picasso for loading into imageviews

--- app/src/main/java/me/michaelhaas/triplist/service/core/model/resolver/ImageResolver.kt
+++ app/src/main/java/me/michaelhaas/triplist/service/core/model/resolver/ImageResolver.kt
@@ -4,17 +4,10 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
-import kotlinx.coroutines.*
 
 open class ImageResolver(val url: Uri) : BaseResolver<Bitmap>() {
 
-    private val job = SupervisorJob()
-    private val scope = CoroutineScope(Dispatchers.Main + job)
-
-    fun resolveInto(view: ImageView) = scope.launch {
-        val bitmap = withContext(Dispatchers.IO) { resolve() }
-        view.setImageBitmap(bitmap)
-    }
+    fun resolveInto(view: ImageView) = Picasso.get().load(url).into(view)
 
     override suspend fun resolve(): Bitmap? = Picasso.get().load(url).get()
 }
